030d8392f68a36707725136dfe34b912a1dea9ed
FriendlyFire <Friendly0Fire@users.noreply.github.com>
2016-05-01 16:40:59 +0000

    Further sped up model viewer updating by splitting updates between hardpoints and mesh and only performing the required changes. Improved DataGridView updating by building the entire collection and then AddRange'ing it in one go. Fixed a few bugs along the way.

--- UTFEditor/UTFFormObserver.cs
+++ UTFEditor/UTFFormObserver.cs
@@ -6,9 +6,16 @@ using System.Windows.Forms;
 
 namespace UTFEditor
 {
+    public enum DataChangedType
+    {
+        Mesh = 1,
+        Hardpoints = 2,
+        All = Mesh | Hardpoints
+    }
+
     public interface UTFFormObserver
     {
-        void DataChanged(TreeNode node, string oldName, object oldData);
+        void DataChanged(DataChangedType changedType);
         void Invalidate();
         void Close();
     }
